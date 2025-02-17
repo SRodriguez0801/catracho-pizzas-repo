f359068469019ee69a2cea9ba0867f2aff474063
SRodriguez0801 <131110417+SRodriguez0801@users.noreply.github.com>
2025-02-16 20:55:47 -0600

    correcion
    tenia los componentes mal importados

--- product-card/src/app/app.component.ts
+++ product-card/src/app/app.component.ts
@@ -1,51 +1,13 @@
 import { Component } from '@angular/core';
-import {
-  AlertController,
-  IonCard,
-  IonCardHeader,
-  IonCardTitle,
-  IonCardContent,
-  IonItem,
-  IonButton,
-  IonLabel, IonApp, IonRouterOutlet, IonCardSubtitle } from '@ionic/angular/standalone';
-import { CommonModule } from '@angular/common';
-
+import { IonApp,IonContent,IonHeader, IonTitle, IonToolbar  } from '@ionic/angular/standalone';
+import { ProductCardComponent } from './product-card/product-card.component';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  standalone: true,
-  imports: [IonCardSubtitle, IonRouterOutlet, IonApp, 
-    CommonModule,
-    IonCard,
-    IonCardHeader,
-    IonCardTitle,
-    IonCardContent,
-    IonItem,
-    IonButton,
-    IonLabel,
-  ],
+  imports: [IonApp, IonContent, IonHeader, IonTitle, IonToolbar, ProductCardComponent],
 })
 export class AppComponent {
-  quantity: number = 1;
-  constructor( private alerController: AlertController) {}
-
-  decreaseQuantity() {
-    if (this.quantity > 1) {
-      this.quantity--;
-    }
-  }
-
-  increaseQuantity() {
-    this.quantity++;
-  }
-
-  async presentAlert() {
-    const alert = await this.alerController.create({
-      header: 'Exito',
-      message: 'Ha agregado el producto al carrito',
-      buttons: ['OK'],
-    });
-
-    await alert.present();
-  }
+  constructor() {}
 }
+
+
--- product-card/src/app/product-card/product-card.component.ts
+++ product-card/src/app/product-card/product-card.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  AlertController,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonItem,
+  IonButton,
+  IonLabel, IonApp, IonRouterOutlet, IonCardSubtitle } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss'],
+  styleUrls: ['./product-card.component.css'],
   standalone: true,
+  imports: [IonCardSubtitle, IonRouterOutlet, IonApp, 
+    CommonModule,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonItem,
+    IonButton,
+    IonLabel,
+  ],
 })
-export class ProductCardComponent  implements OnInit {
+export class ProductCardComponent {
 
-  constructor() { }
+  quantity: number = 1;
+  constructor( private alerController: AlertController) {}
 
-  ngOnInit() {}
+  decreaseQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+  }
+
+  increaseQuantity() {
+    this.quantity++;
+  }
+
+  async presentAlert() {
+    const alert = await this.alerController.create({
+      header: 'Exito',
+      message: 'Ha agregado el producto al carrito',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
 
 }
+
+
+
+
